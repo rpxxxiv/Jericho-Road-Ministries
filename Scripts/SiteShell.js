3efede7e32dd2115bb8f8661748afe31220743bc
Andrew Ferrante <rpxxxiv@gmail.com>
2012-05-26 10:57:48 -0700

    Version Init

---
+++ Scripts/SiteShell.js
@@ -0,0 +1,21 @@
+﻿var toggleSpeed = 1000;
+function NavMenuLoad(ul) {
+    $(ul).each(
+        function () {
+            $(this).hover(
+                function () {
+                    $(this).find('ul').fadeIn(500);
+                },
+                function () {
+                    $(this).find('ul').fadeOut(500);
+                });
+        })
+}
+
+function frameResize(iframe) {
+
+    var frm = $(iframe).attr('id');
+    var h = document.getElementById(frm).contentWindow.document.body.offsetHeight + 'px';
+    document.getElementById(frm).style.height = h;
+}
+
